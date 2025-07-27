11cdae9d3cdb7da5131006d845815c3452e6231d
TigorCoding <146649987+TigorCoding@users.noreply.github.com>
2025-07-27 21:47:06 +0700

    feat(talent-growth): Add model for Tasks,

---
+++ app/models/task.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Task extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare title: string
+
+  @column()
+  declare description: string | null
+
+  @column()
+  declare category: string
+
+  @column()
+  declare priority: 'low' | 'medium' | 'high'
+
+  @column.dateTime()
+  declare deadline: DateTime
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
